544ca1aade7f958778c2bc1ec9efd864d60b5b70
kberaki <berakik@gmail.com>
2018-12-04 11:25:05 -0500

    server.js updated with dynamic

--- server.js
+++ server.js
@@ -2,43 +2,33 @@
 const express = require('express')
 const superagent =require('superagent')
 const app = express()
-const PORT = process.env.PORT || 3000
 const cors = require('cors')
 require('dotenv').config()
-
-
-
+const PORT = process.env.PORT || 3000
 app.use(cors())
-app.get('/location',(req, res)=> 
-{
-  const url= 
-  `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key= ${process.env.GOOGLE_API_KEY}`
+app.get('/location',(req, res)=> {
+  const url= `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.address}&key=${process.env.GOOGLE_API_KEY}`
+  console.log(req.query)
   superagent.get(url)
   .then(result=>{
-    res.send({
-      longtitude:
-      result.body.results[0]
-      .geometry.location.lng,
-      latitude:
-      result.body.results[0]
-      .geometry.location.lat
-    })
+    res.send(new Location(result)) 
   })
-  .catch(err=>res.send('Got an error'))})
-
-app.get('/', (req,res)=>{
-res.send('<h1>This is the Correct route</h1>')
+  .catch(err=>res.send(err))
 })
-
+  app.get('/', (req,res)=>{
+  res.send('<h1>This is the Correct route</h1>')
+})
 app.get('*', (req, res) => 
   res.send('500 error message'))
-
-
-
 app.listen(PORT, ()=>{
   console.log(`Listening on port ${PORT}`)
 })
-
+const Location = function(loc){
+  this.lat = loc.body.results[0]
+  .geometry.location.lat
+  this.log = loc.body.results[0]
+  .geometry.location.lng
+}
 
 
 
